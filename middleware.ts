e67e2ea4bce1d0c67977dc25664aa72ebb6c2634
victorolivaresat <tu_email@ejemplo.com>
2025-11-27 21:53:32 -0500

    refactor: reestructurar middleware para gestionar autenticación y redirección en el dashboard

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,20 @@
-export { auth as middleware } from "@/auth";
+import { auth } from "@/auth";
+import { NextResponse } from "next/server";
+
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+  const isOnDashboard = req.nextUrl.pathname.startsWith("/dashboard");
+
+  if (isOnDashboard && !isLoggedIn) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  return NextResponse.next();
+});
 
 export const config = {
   matcher: ["/dashboard/:path*"],
 };
+
+// Usar Node.js runtime en lugar de Edge
+export const runtime = "nodejs";
