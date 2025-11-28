8762131630b4e258582b8005a83c0835f8cd1603
victorolivaresat <tu_email@ejemplo.com>
2025-11-27 21:46:18 -0500

    refactor: export prisma instance for better accessibility

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -12,6 +12,7 @@ declare global {
 
 const prisma = globalThis.prisma ?? prismaClientSingleton();
 
+export { prisma };
 export default prisma;
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
